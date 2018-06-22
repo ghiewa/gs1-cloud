607656acb145c49f07fc5c84ec3159bfec506b6b
sjoerdsch <33784046+sjoerdsch@users.noreply.github.com>
2018-06-22 14:09:30 +0200

    Version 0.9.0
    First public version

---
+++ credentials.py
@@ -0,0 +1,7 @@
+# Fill in your credentials here
+# the API key can be found in the GS1 Cloud userinterface under 'Account' 
+#
+login = {
+    'email' : 'your_email',
+    'api_key' : 'your_api_key'
+}
