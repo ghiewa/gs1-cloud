78bdc30e4ac3925d725e0280032832e128c96cf3
Sjoerd Schaper <sjoerd.schaper@gmail.com>
2018-08-16 22:30:28 +0200

    Pool size set safer

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Settings for GS1_Cloud_CHECK_GTINS.py and GS1_Cloud_VIEW_GTINS.py
 #
 # Choose the name of the inputfile here
-input_file = "gtins.txt"
+input_file = "10000_gtins.txt"
 
 # Choose output language here
 # You can find an overview of the available languages in the file messages.py
@@ -12,10 +12,10 @@ output_to_screen = False
 
 # Poolsize is the number of threads used by the program
 # It can be set upto 500. Depending on your system and internet connection you might be able to increase it.
-poolsize = 500
+poolsize = 100
 
 # Batchsize is the number of GTINS send to the API per batch
-batchsize = 10000
+batchsize = 1000
 
 # If you have to run the same dataset again due to a break or crash
 # you can set te batch number you want to start with here.
