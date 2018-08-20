53d90dc1f18c55ffdce6451ff6f23808acabb75c
Sjoerd Schaper <sjoerd.schaper@gs1.nl>
2018-08-20 16:32:51 +0200

    Some corrections

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ output_to_screen = False
 
 # Poolsize is the number of threads used by the program
 # It can be set higher depending on your system and internet connection.
-# Please not that GS1 Cloud is still in beta and that the capacity of the API is limited.
-# If errors occur and/or the API doesn't give a 100 % return you can lower the pool size in order to get betteer results.
+# Please note that GS1 Cloud is still in beta and that the capacity of the API is limited.
+# If errors occur and/or the API doesn't give a 100 % return you can lower the pool size in order to get better results.
 poolsize = 100
 
 # Batchsize is the number of GTINS send to the API per batch
