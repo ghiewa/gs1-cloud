f5eff811d8e61bef04955b5e3a52ca0493487f26
Sjoerd Schaper <sjoerd.schaper@gs1.nl>
2018-11-28 09:09:26 +0100

    Intermediate saving of results

--- GS1_Cloud_CHECK_GTINS.py
+++ GS1_Cloud_CHECK_GTINS.py
@@ -300,11 +300,15 @@ if __name__ == "__main__":
         # The main process is waiting for threads to complete
         pool.wait_completion()
         sec_job = round((time.time() - starttime))
-        sec_batch = round((time.time() - starttime_batch))
+        sec_batch = round((time.time() - starttime_batch), 1)
         print("Finished batch %s: %s GTINS after %s (%s responses in %s seconds, %s per second). \n" % (i, len(batches[i]), str(
             datetime.timedelta(seconds=sec_job)), responses_batch, sec_batch, round(responses_batch/max(sec_batch, 1), 1)))
         log.write("Finished batch %s: %s GTINS after %s (%s responses in %s seconds, %s per second). \n" %
                   (i, len(batches[i]), str(datetime.timedelta(seconds=sec_job)), responses_batch, sec_batch, round(responses_batch/max(sec_batch, 1), 1)))
+        output.flush()
+        output_invalid.flush()
+        active_gtins.flush()
+        log.flush()
 
     gtins_in_input = len(data_unique)
 
